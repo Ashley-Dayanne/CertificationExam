7d331e5a1fa48add9811edd4945ee21697967e5a
Ashley-Dayanne <ux24ii240@ux.edu.mx>
2026-05-21 22:58:39 -0600

    Add files via upload

---
+++ Day27.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("5");
+
+        System.out.println("4 3");
+        System.out.println("-1 0 4 2");
+
+        System.out.println("5 2");
+        System.out.println("0 -1 2 1 4");
+
+        System.out.println("6 3");
+        System.out.println("-1 0 1 2 3 4");
+
+        System.out.println("7 5");
+        System.out.println("-1 0 1 2 3 4 5");
+
+        System.out.println("3 1");
+        System.out.println("-1 0 1");
+    }
+}
