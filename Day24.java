8a3a1bd9702944d3a0ff1484bb497475deb90c19
Ashley-Dayanne <ux24ii240@ux.edu.mx>
2026-05-21 23:14:30 -0600

    Add files via upload

---
+++ Day24.java
@@ -0,0 +1,26 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Solution {
+
+    public static Node removeDuplicates(Node head) {
+        Node current = head;
+
+        while (current != null && current.next != null) {
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+}
